a3c0a3ef3ce82c64ae4ee97f3380b83f6af72885
GeniusPudding <r08922107@ntu.edu.tw>
2021-10-04 04:34:28 +0000

    建立基本樣板中

---
+++ pages/dashboard.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { useState } from "react";
+
+// import Date from "../components/date";
+
+// For Static Generation, export "getStaticProps" (getStaticProps can only be exported from a page)
+export async function getStaticProps() {
+
+  return {
+    props: {
+      
+    },
+  };
+}
+
+// First page after login
+export default function DashBoard(props) {
+
+    return (
+        <div></div>
+        
+    )
+}
