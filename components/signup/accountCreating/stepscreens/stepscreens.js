a9461f4b8344020b71e6c216372ab293bbb6f63a
GeniusPudding <r08922107@ntu.edu.tw>
2021-10-10 11:31:06 +0800

    why npm ERR

--- components/signup/accountCreating/stepscreens/stepscreens.js
+++ components/signup/accountCreating/stepscreens/stepscreens.js
@@ -1,21 +1,41 @@
 import { useState } from "react";
 import styles1 from "./stepscreens1.module.css"
 import styles2 from "./stepscreens2.module.css"
+import firebase from "firebase/app"
+import "firebase/auth"
+import initFirebase from '../../../../firebase/initFirebase'
 export default function RightStepScreens({ step }) {
     const two = 2 & step
     const one = 1 & step
     const [connected, setConnected] = useState(true)
-
+    initFirebase()
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("handleSubmit",e)
+        console.log("handleSubmit.target.elements['ORCIDid'].value",e.target.elements['ORCIDid'].value)
+        console.log("handleSubmit.target.elements['lastname'].value",e.target.elements['lastname'].value)
+        console.log("handleSubmit.target.elements['firstname'].value",e.target.elements['firstname'].value)
+        console.log("handleSubmit.target.elements['email'].value",e.target.elements['email'].value)
+        console.log("handleSubmit.target.elements['password'].value",e.target.elements['password'].value)
+        
+        firebase.auth().createUserWithEmailAndPassword(user.email, user.pwd)
+        .then(u => {
+          // 記錄相關資訊到 firebase realtime database
+          console.log('u.uid:',u.uid)
+
+          database.ref(u.uid).set({
+            signup: now,
+            email: user.email
+          }).then(() => {
+            // 儲存成功後顯示訊息
+            message.innerHTML = 'User created successfully';
+          });
+        }).catch(err => {
+          // 註冊失敗時顯示錯誤訊息
+          message.innerHTML = err.message;
+        });
 
       };
-      const handleReset = (e) => {
-        e.preventDefault();
-        console.log("handleReset",e)
 
-      };
 //自定義註冊帳號參考  https://www.oxxostudio.tw/articles/201905/firebase-simple-signup.html
 
     return (
@@ -51,7 +71,7 @@ export default function RightStepScreens({ step }) {
                             //step 2
                             <div>
                                 <div className={styles2.personal}>Personal Information</div>
-                                <form onSubmit={handleSubmit} onReset={handleReset}>
+                                <form onSubmit={handleSubmit}>
                                     <div>
                                     <input type="text" placeholder="ORCID ID" id="ORCIDid" name="ORCID ID" className={styles2.rec64}/>
                                     <input type="text" placeholder="Last Name" id="lastname" name="Last Name" className={styles2.rec65}/>
