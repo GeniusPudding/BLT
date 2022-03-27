19d425825c6c213e148858284ea6508e591cc390
GeniusPudding <r08922107@ntu.edu.tw>
2022-03-27 11:56:36 +0800

    why can't login

---
+++ components/personal/nav/nav_team.js
@@ -0,0 +1,12 @@
+import styles from './nav.module.css'
+
+export default function NavTeam() {
+    
+    return (
+        <div className={styles.team}>
+            Team Management
+        </div>
+
+
+    )
+  }
